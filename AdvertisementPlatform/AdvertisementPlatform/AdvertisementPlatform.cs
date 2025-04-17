7a40f49fe5fd2e38591a9f43e57639d54e27b899
Vuvkings <vova.scholokhov@gmail.com>
2025-04-17 22:30:53 +0300

    Add files via upload

---
+++ AdvertisementPlatform/AdvertisementPlatform/AdvertisementPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementPlatform
+{
+    public class AdvertismentPlatform
+    {
+        private Dictionary<string, List<string>> advertismentByLocation;
+        public AdvertismentPlatform()
+        {
+            advertismentByLocation = new();
+        }
+        public void AddPlatform(string advertismentLocation)
+        {
+            var elements = "Яндекс.Директ:/ru/svrd/pervik".Split(':');
+
+            var locations = elements[1].Split('/');
+        }
+        public List<string> GetPlatform(string location)
+        {
+            return new List<string>();
+        }
+
+    }
+}
---
+++ AdvertisementPlatform/AdvertisementPlatform/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdvertisementPlatform;
+Console.WriteLine("Hello, World!");
+//var n = Convert.ToInt32(Console.ReadLine());
+int n = 10;
+var advertismentPlatform = new AdvertismentPlatform();
+
+
+for (int i = 0; i < n; i++)
+{
+    var value = Console.ReadLine();
+    advertismentPlatform.AddPlatform(value);
+}
+
+var location = Console.ReadLine();
+var advertismentPlatforms = advertismentPlatform.GetPlatform(location);
+
+for (int i = 0; i < advertismentPlatforms.Count; i++)
+{
+    Console.WriteLine(advertismentPlatforms[i]);
+}
