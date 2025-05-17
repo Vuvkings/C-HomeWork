f56f693f39f2d803cc5897f0851c9743dc07cb0e
Vuvkings <vova.scholokhov@gmail.com>
2025-05-17 14:38:23 +0300

    Add files via upload

---
+++ stack/stack/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Stack<int> stack = new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+Console.WriteLine(stack.Peek()); // Вывод: 3
+Console.WriteLine(stack.Pop()); // Вывод: 3
+Console.WriteLine(stack.Pop()); // Вывод: 2
+Console.WriteLine(stack.Pop()); // Вывод: 1
